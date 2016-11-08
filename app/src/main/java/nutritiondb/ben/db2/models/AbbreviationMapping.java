342e8c6e0c55e9b0941c6e399410d4c3f3ff4e04
Ben Ebsworth <ben.ebsworth@gmail.com>
2016-11-09 07:07:28 +1100

    adding unit mapping

--- app/src/main/java/nutritiondb/ben/db2/models/AbbreviationMapping.java
+++ app/src/main/java/nutritiondb/ben/db2/models/AbbreviationMapping.java
@@ -233,6 +233,8 @@ public class AbbreviationMapping {
 
         tmp.clear();
         // ------- DEFAULT UNITS ------------
+        // Units should be dynamic based on the size of the value relative to the default
+        // so for example if you have 0.001g then convert to 1mg etc.
         //Energy
         tmp.put("ENERC", "kJ");
         tmp.put("ENERC_KCAL", "kCal");
@@ -286,105 +288,105 @@ public class AbbreviationMapping {
         tmp.put("FAPU", "g");
         tmp.put("FASAT", "g");
         tmp.put("FATRN", "g");
-        tmp.put("F4D0", "4:0"); //%T or g?
-        tmp.put("F6D0", "6:0");
-        tmp.put("F8D0", "8:0");
-        tmp.put("F10D0", "10:0");
-        tmp.put("F10D1", "10:1");
-        tmp.put("F12D0", "12:0");
-        tmp.put("F13D0", "13:0");
-        tmp.put("F14D0", "14:0");
-        tmp.put("F14D1", "14:1");
-        tmp.put("F15D0", "15:0");
-        tmp.put("F15D1", "15:1");
-        tmp.put("F16D0", "16:0");
-        tmp.put("F16D1", "16:1 undifferentiated");
-        tmp.put("F16D1T", "16:0T"); //trans
-        tmp.put("F16D1T", "16:0C"); //cis
-        tmp.put("F17D0", "17:0");
-        tmp.put("F17D1", "17:1");
-        tmp.put("F18D0", "18:0");
-        tmp.put("F18D1C", "18:1 C");
-        tmp.put("F18D1TN7", "18:1 T7");
-        tmp.put("F18D1", "18:1 undifferentiated");
-        tmp.put("F18D2", "18:2 undifferentiated");
-        tmp.put("F18D2CN6", "18:2 c n-6");
-        tmp.put("F18D2TT", "18:2 TT");
-        tmp.put("F18D2I", "18:2 I");
-        tmp.put("F18D2N6", "18:2 n-6");
-        tmp.put("F18D3", "18:3 undifferentiated");
-        tmp.put("F18D3T", "18:3 T");
-        tmp.put("F18D3I", "18:3 I");
-        tmp.put("F18D3N6", "18:3 n-6");
-        tmp.put("F18D3N3F", "18:3 n-3"); // per unit
-        tmp.put("F18D3N3", "18:3 n-3"); // per %
-        tmp.put("F18D4", "18:4");
-        tmp.put("F18D4N3", "18:4 n-3");
-        tmp.put("F19D0", "19:0");
-        tmp.put("F20D0", "20:0");
-        tmp.put("F20D1", "20:1");
-        tmp.put("F20D1N11F", "20:1 n-11");
-        tmp.put("F20D2N6", "20:2 n-6 c,c");
-        tmp.put("F20D3N3", "20:2 n-3");
-        tmp.put("F20D3", "20:3 undifferentiated");
-        tmp.put("F20D4", "20:4 undifferentiated");
-        tmp.put("F20D4N6", "20:4 n-6");
-        tmp.put("F20D5", "20:5 n-3 (EPA)");
-        tmp.put("F21D0", "21:0");
-        tmp.put("F22D0", "22:0");
-        tmp.put("F22D1", "22:1 undifferentiated");
-        tmp.put("F22D1C", "22:1 c");
-        tmp.put("F22D1T", "22:1 t");
-        tmp.put("F22D3", "20:2 n-3");
-        tmp.put("F22D5N3", "22:5 n-3 (DPA)");
-        tmp.put("F22D6", "22:6");
-        tmp.put("F22D6N3", "22:6 n-3 (DHA)");
-        tmp.put("F23D0", "23:0");
-        tmp.put("F24D0", "24:0");
-        tmp.put("F24D1", "24:1");
-        tmp.put("F24D1C", "24:1 c");
-        tmp.put("LCW3TOTAL", "");
-        tmp.put("FAUNDIFF", "Undifferentiated fatty acids");
+        tmp.put("F4D0", "mg"); //%T or g?
+        tmp.put("F6D0", "mg");
+        tmp.put("F8D0", "mg");
+        tmp.put("F10D0", "mg");
+        tmp.put("F10D1", "mg");
+        tmp.put("F12D0", "mg");
+        tmp.put("F13D0", "mg");
+        tmp.put("F14D0", "mg");
+        tmp.put("F14D1", "mg");
+        tmp.put("F15D0", "mg");
+        tmp.put("F15D1", "mg");
+        tmp.put("F16D0", "mg");
+        tmp.put("F16D1", "mg");
+        tmp.put("F16D1T", "mg"); //trans
+        tmp.put("F16D1T", "mg"); //cis
+        tmp.put("F17D0", "mg");
+        tmp.put("F17D1", "mg");
+        tmp.put("F18D0", "mg");
+        tmp.put("F18D1C", "mg");
+        tmp.put("F18D1TN7", "mg");
+        tmp.put("F18D1", "mg");
+        tmp.put("F18D2", "mg");
+        tmp.put("F18D2CN6", "mg");
+        tmp.put("F18D2TT", "mg");
+        tmp.put("F18D2I", "mg");
+        tmp.put("F18D2N6", "mg");
+        tmp.put("F18D3", "mg");
+        tmp.put("F18D3T", "mg");
+        tmp.put("F18D3I", "mg");
+        tmp.put("F18D3N6", "mg");
+        tmp.put("F18D3N3F", "mg"); // per unit
+        tmp.put("F18D3N3", "mg"); // per %
+        tmp.put("F18D4", "mg");
+        tmp.put("F18D4N3", "mg");
+        tmp.put("F19D0", "mg");
+        tmp.put("F20D0", "mg");
+        tmp.put("F20D1", "mg");
+        tmp.put("F20D1N11F", "mg");
+        tmp.put("F20D2N6", "mg");
+        tmp.put("F20D3N3", "mg");
+        tmp.put("F20D3", "mg");
+        tmp.put("F20D4", "mg");
+        tmp.put("F20D4N6", "mg");
+        tmp.put("F20D5", "mg");
+        tmp.put("F21D0", "mg");
+        tmp.put("F22D0", "mg");
+        tmp.put("F22D1", "mg");
+        tmp.put("F22D1C", "mg");
+        tmp.put("F22D1T", "mg");
+        tmp.put("F22D3", "mg");
+        tmp.put("F22D5N3", "mg");
+        tmp.put("F22D6", "mg");
+        tmp.put("F22D6N3", "mg");
+        tmp.put("F23D0", "mg");
+        tmp.put("F24D0", "mg");
+        tmp.put("F24D1", "mg");
+        tmp.put("F24D1C", "mg");
+        tmp.put("LCW3TOTAL", "mg");
+        tmp.put("FAUNDIFF", "mg");
 
         //acids
-        tmp.put("ACEAC", "Acetic acid");
-        tmp.put("CITAC", "Citric acid");
-        tmp.put("FUMAC", "Fumaric acid");
-        tmp.put("LACAC", "Lactic acid");
-        tmp.put("MALAC", "Malic acid");
-        tmp.put("OXALAC", "Oxalic acid");
-        tmp.put("PROPAC", "Propionic acid");
-        tmp.put("QUINAC", "Quinic acid");
-        tmp.put("SHIKAC", "Shikimic acid");
-        tmp.put("SUCAC", "Succinic acid");
-        tmp.put("TARAC", "Tartaric acid");
+        tmp.put("ACEAC", "g");
+        tmp.put("CITAC", "g");
+        tmp.put("FUMAC", "g");
+        tmp.put("LACAC", "g");
+        tmp.put("MALAC", "g");
+        tmp.put("OXALAC", "g");
+        tmp.put("PROPAC", "g");
+        tmp.put("QUINAC", "g");
+        tmp.put("SHIKAC", "g");
+        tmp.put("SUCAC", "g");
+        tmp.put("TARAC", "g");
 
 
 //        tmp.put("", "");
         //Minerals & metals
-        tmp.put("AL", "Aluminium");
-        tmp.put("AS", "Arsenic");
-        tmp.put("CA", "Calcium (Ca)");
-        tmp.put("CD", "Cadmium (Cd)");
-        tmp.put("CU", "Copper (Cu)");
-        tmp.put("CO", "Cobalt (Co)");
-        tmp.put("CR", "Chromium (Cr)");
-        tmp.put("FD", "Fluoride (F)");
-        tmp.put("FE", "Iron (Fe)");
-        tmp.put("HG", "Mercury (Hg)");
-        tmp.put("ID", "Iodine (I)");
-        tmp.put("K", "Potassium (K)");
-        tmp.put("MG", "Magnesium (Mg)");
-        tmp.put("MN", "Manganese (Mn)");
-        tmp.put("M0", "Molybdenum (Mo)");
-        tmp.put("NA", "Sodium (Na)");
-        tmp.put("NI", "Nickel (Ni)");
-        tmp.put("P", "Phosphorus (P)");
-        tmp.put("PB", "Lead (Pb)");
-        tmp.put("S", "Sulphur (S)");
-        tmp.put("SE", "Selenium (Se)");
-        tmp.put("SN", "Tin (Sn)");
-        tmp.put("ZN", "Zinc (Zn)");
+        tmp.put("AL", "ug");
+        tmp.put("AS", "ug");
+        tmp.put("CA", "mg");
+        tmp.put("CD", "ug");
+        tmp.put("CU", "mg");
+        tmp.put("CO", "ug");
+        tmp.put("CR", "ug");
+        tmp.put("FD", "ug");
+        tmp.put("FE", "mg");
+        tmp.put("HG", "ug");
+        tmp.put("ID", "ug");
+        tmp.put("K", "mg");
+        tmp.put("MG", "mg");
+        tmp.put("MN", "mg");
+        tmp.put("M0", "ug");
+        tmp.put("NA", "mg");
+        tmp.put("NI", "ug");
+        tmp.put("P", "mg");
+        tmp.put("PB", "ug");
+        tmp.put("S", "mg");
+        tmp.put("SE", "ug");
+        tmp.put("SN", "ug");
+        tmp.put("ZN", "mg");
         //Vitamins
         tmp.put("BIOT", "Biotin (B7)");
         tmp.put("B1", "Thiamin (B1)");
